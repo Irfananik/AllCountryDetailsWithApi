5c0067ff33d2f98158fc35d0deb627ac76a807c6
Golam Morshed Bhuiyan <anik.cse10@gamil.com>
2023-06-30 22:25:09 +0600

    some desing changed and footer added

--- js/app.js
+++ js/app.js
@@ -23,40 +23,54 @@ const displayCountriesInfo = (getCountries) => {
         <div class="bg-red-400 p-4 rounded-lg shadow-lg">
             <img src="${getCountry.flags.png}" alt="">
             <h2 class="text-lg text-white font-bold mb-2">Name: ${getCountry.name.common}</h2>
-            <p class="text-gray-300">Capital: ${getCountry.capital ? getCountry.capital : 'No exist'}</p>
+            
             <button onclick="loadCountryDetails('${getCountry.cca2}')"
                 class="bg-red-700 text-white px-4 py-2 rounded-md hover:bg-red-800 focus:outline-none mt-4">Details</button>
+            <button onclick="scrollToTop()"
+                class="bg-red-700 text-white px-4 py-2 rounded-md hover:bg-red-800 focus:outline-none mt-4"><i class="fa-solid fa-arrow-up"></i></i></button>
         </div>
         `
         countriesContainer.appendChild(getCountriesDiv)
     })
 }
 
-//show country details load
+loadCountriesInfo()
+
+//country details load
 const loadCountryDetails = (data) => {
     const url = `https://restcountries.com/v3.1/alpha?codes=${data}`
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            displayCountryDetails(data[0])
+            displayCountryDetails(data[0], scrollToTop)
         })
         .catch(error => {
             console.log(error)
         })
 }
 
+//display country details
 const displayCountryDetails = (getCountryDetails) => {
     const countryDetailsContainer = document.getElementById('country-details-container')
     const getCountryDetailsDiv = document.createElement('div')
     getCountryDetailsDiv.classList.add('col')
     getCountryDetailsDiv.innerHTML = `
-    <div class="bg-red-400 p-4 rounded-lg shadow-lg">
+    <div class="bg-red-400 p-4 rounded-lg shadow-lg me-4">
         <img src="${getCountryDetails.flags.png}" alt="">
         <h2 class="text-lg text-white font-bold mb-2">Name: ${getCountryDetails.name.common}</h2>
+        <p class="text-gray-300">Official Name: ${getCountryDetails.name.official}</p>
         <p class="text-gray-300">Capital: ${getCountryDetails.capital ? getCountryDetails.capital : 'No exist'}</p>
+        <p class="text-gray-300">Region: ${getCountryDetails.region}</p>
+        <p class="text-gray-300">independent Status: ${getCountryDetails.independent}</p>
     </div>
     `
     countryDetailsContainer.appendChild(getCountryDetailsDiv)
 }
 
-loadCountriesInfo()
+// Scroll to the top of the page function
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
