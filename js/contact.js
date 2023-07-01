72c1c1f4e12741fca5e409cab564d6a89c11c465
Golam Morshed Bhuiyan <anik.cse10@gamil.com>
2023-07-01 15:02:02 +0600

    contact form completed with functionality

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+const contactFormDataSubmited = () => {
+    const nameField = document.getElementById('name')
+    const emailField = document.getElementById('email')
+    const commentField = document.getElementById('comment')
+
+    const nameFieldValue = nameField.value
+    const emailFieldValue = emailField.value
+    const commentFieldValue = commentField.value
+
+    if (nameFieldValue && emailFieldValue && commentFieldValue === nameField.value, emailField.value, commentField.value) {
+        alert("Your comment has been succesfully saved!")
+    } else if (nameFieldValue && emailFieldValue && commentFieldValue !== nameField.value, emailField.value, commentField.value) {
+        alert("Please enter your valuable comment first!")
+    }
+    else {
+        alert("Please enter your valuable comment first!")
+    }
+
+    nameField.value = ''
+    emailField.value = ''
+    commentField.value = ''
+
+    //console.log(nameFieldValue, emailFieldValue, commentFieldValue)
+}
