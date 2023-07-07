bc787156aae00b5fc35a1ed0765b707c0d776a6c
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2023-07-07 14:34:20 +0600

    site updated done

--- js/contact.js
+++ js/contact.js
@@ -1,24 +1,31 @@
 const contactFormDataSubmited = () => {
-    const nameField = document.getElementById('name')
-    const emailField = document.getElementById('email')
-    const commentField = document.getElementById('comment')
+  const nameField = document.getElementById("name");
+  const emailField = document.getElementById("email");
+  const commentField = document.getElementById("comment");
 
-    const nameFieldValue = nameField.value
-    const emailFieldValue = emailField.value
-    const commentFieldValue = commentField.value
+  const nameFieldValue = nameField.value;
+  const emailFieldValue = emailField.value;
+  const commentFieldValue = commentField.value;
 
-    if (nameFieldValue && emailFieldValue && commentFieldValue === nameField.value, emailField.value, commentField.value) {
-        alert("Your comment has been succesfully saved!")
-    } else if (nameFieldValue && emailFieldValue && commentFieldValue !== nameField.value, emailField.value, commentField.value) {
-        alert("Please enter your valuable comment first!")
-    }
-    else {
-        alert("Please enter your valuable comment first!")
-    }
+  if (
+    (nameFieldValue && emailFieldValue && commentFieldValue === nameField.value,
+    emailField.value,
+    commentField.value)
+  ) {
+    alert("Your comment has been succesfully saved!");
+  } else if (
+    (nameFieldValue && emailFieldValue && commentFieldValue !== nameField.value,
+    emailField.value,
+    commentField.value)
+  ) {
+    alert("Please enter your valuable comment first!");
+  } else {
+    alert("Please enter your valuable comment first!");
+  }
 
-    nameField.value = ''
-    emailField.value = ''
-    commentField.value = ''
+  nameField.value = "";
+  emailField.value = "";
+  commentField.value = "";
 
-    //console.log(nameFieldValue, emailFieldValue, commentFieldValue)
-}
+  //console.log(nameFieldValue, emailFieldValue, commentFieldValue)
+};
